d7ce5ee8102da7efbb9cbd908efd766eae2f0187
VladimirTarasovets <tarasovets21@gmail.com>
2022-06-28 12:59:07 +0300

    add serialization Array

--- src/serialization/WriteObject.java
+++ src/serialization/WriteObject.java
@@ -6,15 +6,32 @@ import java.io.ObjectOutputStream;
 
 public class WriteObject {
     public static void main(String[] args) {
-        Person person1 = new Person(1, "Bob");
-        Person person2 = new Person(2, "Mike");
+
+/*        {
+            Person person1 = new Person(1, "Bob");
+            Person person2 = new Person(2, "Mike");
+
+            try {
+                FileOutputStream fos = new FileOutputStream("people.bin");
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+                oos.writeObject(person1);
+                oos.writeObject(person2);
+                oos.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+*/
+        Person[] people = { new Person(1, "Bob"),
+                new Person(2, "Mike"),new Person(3, "Nick")};
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
+            oos.writeObject(people);
             oos.close();
 
         } catch (IOException e) {
