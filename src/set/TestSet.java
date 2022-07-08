47c738a06f6876c594d3635b98e22f08021211b7
VladimirTarasovets <tarasovets21@gmail.com>
2022-07-08 16:08:22 +0300

    add Set

---
+++ src/set/TestSet.java
@@ -0,0 +1,41 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestSet {
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(0);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+
+        //union
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println(union);
+
+        //intersection
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println(intersection);
+
+        //difference
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println(difference);
+
+    }
+}
